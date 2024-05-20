fdb181cb3305937dd38873884fa8b814df860cad
Nail <mazhitovnail@gmail.com>
2024-05-20 16:47:44 +0300

    Commit test

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,4 +2,9 @@ package jm.task.core.jdbc.util;
 
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String
+    private static final String
+    private static final String
+    private static final String
+
 }
